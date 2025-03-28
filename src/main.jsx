7347d8092920d48f22d54413dca7ad36e7321ce7
Adeyemi Pelumi <adeyemipelumi1980@gmail.com>
2025-03-28 20:27:11 +0100

    new

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 // import { AuthProvider } from "./Auth/AuthContext";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 import { StrictMode } from "react";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <StrictMode>
     <App />
-  </BrowserRouter>
+  </StrictMode>
 );
 
 // import React from "react";
