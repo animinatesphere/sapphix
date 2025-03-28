badc76eed7718a516b5cb5c0def2a92f3ef61ca9
Adeyemi Pelumi <adeyemipelumi1980@gmail.com>
2025-03-28 20:25:57 +0100

    new

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,9 @@ import { BrowserRouter } from "react-router-dom";
 import { StrictMode } from "react";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <BrowserRouter>
     <App />
-  </StrictMode>
+  </BrowserRouter>
 );
 
 // import React from "react";
